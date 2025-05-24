0998d1d2f26297c93421644a31529b56d72ee858
darwingb21 <darwingb18@gmail.com>
2025-05-23 23:22:22 -0400

    Inicial API, funcional

---
+++ app/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import models, schemas, crud
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/usuarios/", response_model=schemas.Usuario)
+def crear_usuario(usuario: schemas.UsuarioCreate, db: Session = Depends(get_db)):
+    db_usuario = crud.get_usuario_por_email(db, email=usuario.email)
+    if db_usuario:
+        raise HTTPException(status_code=400, detail="Email ya registrado")
+    return crud.crear_usuario(db=db, usuario=usuario)
+
+@app.get("/usuarios/", response_model=list[schemas.Usuario])
+def leer_usuarios(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    usuarios = crud.get_usuarios(db, skip=skip, limit=limit)
+    return usuarios
+
+@app.get("/usuarios/{usuario_id}", response_model=schemas.Usuario)
+def leer_usuario(usuario_id: int, db: Session = Depends(get_db)):
+    db_usuario = crud.get_usuario(db, usuario_id=usuario_id)
+    if db_usuario is None:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    return db_usuario
